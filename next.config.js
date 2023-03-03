f0a52dda74ebca798e2a5fb3b511c35019dceb06
Maryam <89831604+MaryamEsmaily@users.noreply.github.com>
2023-03-03 13:37:50 +0330

    fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    REACT_APP_API_ADDRESS: process.env.REACT_APP_API_ADDRESS,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
